815f6e055eb133735347cb849535c334c1f80832
spizzleayo <rajiayomikun@gmail.com>
2022-05-27 19:05:03 +0000

    Update util.js

--- src/util.js
+++ src/util.js
@@ -2,7 +2,7 @@
 
 
  const smallImage = (imagePath, size) => {
-  const image = imagePath.match(/media/)
+  const image = imagePath.match(/media\/screenshots/)
   ? imagePath.replace(
     "media/screenshots",
     `media/resize/${size}/-/screenshots`)
